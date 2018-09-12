ba1f3dee7636298e5cb4a46599be00b3a302671f
Tysheba <tmorgan2dev@gmail.com>
2018-09-12 17:57:19 -0400

    completed code for concert-this

--- liri.js
+++ liri.js
@@ -6,6 +6,7 @@ var keys = require("./keys")
 var fs = require('fs')
 var request = require('request')
 var Spotify = require('node-spotify-api');
+var moment = require('moment')
 var userCommand = process.argv[2]
 var userCommand2 = process.argv.slice(3);
 
@@ -13,11 +14,32 @@ var userCommand2 = process.argv.slice(3);
 var spotify = new Spotify(keys.spotify);
 
 
-
 // Make it so liri.js can take in one of the following commands:
 
-// concert-this
-// spotify-this-song
+// 1. concert-this
+// Function for running concert search using the Bands in Town Artist Events API
+var getConcerts = function () {
+    var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    request(URL, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //   console.log('body:', JSON.parse(body));
+        console.log ("Upcoming Events for " + artist + "\n")
+        for (i = 0; i < 6; i++) {
+            var bandsInfo = JSON.parse(body)
+            var venueName = bandsInfo[i].venue.name
+            var venueLocation = (bandsInfo[i].venue.city + ", " + bandsInfo[i].venue.region)
+            var eventDate = moment(bandsInfo[i].datetime).format('L')
+            
+            console.log("Venue Name: " + venueName)
+            console.log("Location: " + venueLocation)
+            console.log("Date of Event: " + eventDate)
+            console.log("-----------------------------------");
+        }
+
+    });
+}
+// 2. spotify-this-song
 // Function for running a Spotify search - Command is spotify-this-song
 var getSpotify = function (songName) {
     if (songName === undefined) {
@@ -36,10 +58,11 @@ var getSpotify = function (songName) {
             }
 
             var songs = data.tracks.items;
+            console.log(data)
 
             for (i = 0; i < songs.length; i++) {
                 var songs = data.tracks.items;
-                console.log("artist(s): " + songs[i].artists[0].name);
+                console.log("artist(s): " + songs[i].artists[0].name);//try to get all artist names
                 console.log("song name: " + songs[i].name);
                 console.log("preview link: " + songs[i].preview_url);
                 console.log("album: " + songs[i].album.name);
@@ -58,14 +81,10 @@ var getSpotify = function (songName) {
 switch (userCommand) {
     case "concert-this":
         console.log("concert")
-        var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-        request(URL, function (error, response, body) {
-            console.log('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
-        });
+        var artist = userCommand2;
+        getConcerts();
         break;
-    case "spotify-this-song":
+    case "C":
         var songName = userCommand2;
         getSpotify();
         break;
