cf5a1a6405a844c4f11fba5a23fcb1f0b03e1960
Tysheba <tmorgan2dev@gmail.com>
2018-09-14 15:15:02 -0500

    add read file function

--- liri.js
+++ liri.js
@@ -26,13 +26,13 @@ var getConcerts = function () {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         //   console.log('body:', JSON.parse(body));
-        console.log ("Upcoming Events for " + artist + "\n")
+        console.log("Upcoming Events for " + artist + "\n")
         for (i = 0; i < 6; i++) {
             var bandsInfo = JSON.parse(body)
             var venueName = bandsInfo[i].venue.name
             var venueLocation = (bandsInfo[i].venue.city + ", " + bandsInfo[i].venue.region)
             var eventDate = moment(bandsInfo[i].datetime).format('L')
-            
+
             console.log(chalk.yellow("Venue Name: ") + venueName)
             console.log(chalk.yellow("Location: ") + venueLocation)
             console.log(chalk.yellow("Date of Event: ") + eventDate)
@@ -65,9 +65,9 @@ var getSpotify = function (songName) {
             for (i = 0; i < songs.length; i++) {
                 var songs = data.tracks.items;
                 console.log(chalk.blue("artist(s): ") + songs[i].artists[0].name);//try to get all artist names
-                console.log(chalk.blue ("song name: ") + songs[i].name);
-                console.log(chalk.blue ("preview link: " )+ songs[i].preview_url);
-                console.log(chalk.blue ("album: ") + songs[i].album.name);
+                console.log(chalk.blue("song name: ") + songs[i].name);
+                console.log(chalk.blue("preview link: ") + songs[i].preview_url);
+                console.log(chalk.blue("album: ") + songs[i].album.name);
                 console.log("-----------------------------------");
             }
         }
@@ -84,7 +84,7 @@ var getMovie = function (movie) {
     }
     var URL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json&apikey=trilogy"
     // "https://www.omdbapi.com/?s=" + movie + "&apikey=trilogy"
-    
+
     request(URL, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
@@ -93,22 +93,35 @@ var getMovie = function (movie) {
         var movieThis = movieInfo
         // console.log(movieThis);
         console.log(`
-        ${chalk.green ("Movie Title: ")} ${movieThis.Title}
-        ${chalk.green ("Year : ")} ${movieThis.Year}
-        ${chalk.green ("IMDB Rating : ")} ${movieThis.Ratings[0].Value}
-        ${chalk.green ("Rotten Tomatoes Rating: ")} ${movieThis.Ratings[1].Value}
-        ${chalk.green ("Country where the movie was produced: ")} ${movieThis.Country}
-        ${chalk.green ("Language of the movie: ")} ${movieThis.Language}
-        ${chalk.green ("Movie Plot: ") } ${movieThis.Plot}
-        ${chalk.green ("Actors in the movie: ") } ${movieThis.Actors}
+        ${chalk.green("Movie Title: ")} ${movieThis.Title}
+        ${chalk.green("Year : ")} ${movieThis.Year}
+        ${chalk.green("IMDB Rating : ")} ${movieThis.Ratings[0].Value}
+        ${chalk.green("Rotten Tomatoes Rating: ")} ${movieThis.Ratings[1].Value}
+        ${chalk.green("Country where the movie was produced: ")} ${movieThis.Country}
+        ${chalk.green("Language of the movie: ")} ${movieThis.Language}
+        ${chalk.green("Movie Plot: ")} ${movieThis.Plot}
+        ${chalk.green("Actors in the movie: ")} ${movieThis.Actors}
         ____________________________________________` )
-        
+
         // console.log(movieInfo)
 
     });
     console.log("movie")
 }
-// do-what-it-says
+// 4. do-what-it-says
+// Take the text inside of random.txt and then use it to call one of LIRI's commands
+function doWhatever() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+
+        // We will then print the contents of data
+        console.log(data);
+    });
+}
 
 
 switch (userCommand) {
@@ -123,10 +136,11 @@ switch (userCommand) {
         break;
     case "movie-this":
         var movie = userCommand2;
-        getMovie ();
+        getMovie();
         break;
     case "do-what-it-says":
         console.log("whatever you want")
+        doWhatever ();
         break;
     default:
         console.log("Enter a valid command")
